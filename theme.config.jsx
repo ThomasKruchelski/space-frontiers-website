ca8cc0519ec3db51d6c07cf84d57598a1cef6c7a
Thomas Kruchelski <thomas@lunnar.team>
2023-08-16 22:12:10 -0300

    Add documentation pages

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,10 @@
 export default {
     logo: <span>Simple Space System</span>,
     project: {
-      link: 'https://github.com/shuding/nextra'
+      link: 'https://github.com/ThomasKruchelski/simple-space-system.git'
+    },
+    search: {
+        placeholder:"Faça sua busca..."
     }
     // ... other theme options
   }
